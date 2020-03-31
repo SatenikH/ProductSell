d4cba107891f381447a7928fb40e670ba251f9ed
hhar.satenik <hhar.satenik@gmail.com>
2020-03-31 20:35:22 +0300

    AddCash

---
+++ ProductSell/Model/Cash.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductSell.Model
+{
+    public class Cash
+    {
+        ProductContext db = new ProductContext();
+        public int Number { get; set; }
+        public Seller Seller { get; set; }
+        public Queue<Cart> Queue { get; set; }
+        public int MaxQueueCount { get; set; }
+        public int ExitCustomer { get; set; }
+        public bool IsModel { get; set; }
+        public object Enqueue { get; set; }
+
+        public Cash(int number, Seller seller)
+        {
+            Number = number;
+            Seller = seller;
+            Queue = new Queue<Cart>();
+            IsModel = true;
+        }
+        public void Unqueue(Cart Cart)
+        {
+            if (Queue.Count <= MaxQueueCount)
+                Queue.Enqueue(Cart);
+            ExitCustomer++;
+        }
+        public decimal Dequeue()
+        {
+            decimal sum = 0;
+
+            var cart = Queue.Dequeue();
+            if (cart != null)
+            {
+                var check = new Check()
+                {
+                    SellerID = Seller.SellerID,
+                    Seller = Seller,
+                    CustomerID = cart.Customer.CustomerID,
+                    Customer = cart.Customer,
+                    Created = DateTime.Now
+                };
+                if (!IsModel)
+                {
+                    db.Checks.Add(check);
+                    db.SaveChanges();
+                }
+                else
+                    check.CheckID = 0;
+
+                var sells = new List<Sell>();
+
+                foreach (Product product in cart)
+                {
+                    if (product.Count >0)
+                    {
+                        var sell = new Sell()
+                        {
+                            CheckID = check.CheckID,
+                            Check = check,
+                            ProductID = product.ProductID,
+                            Product = product
+                        };
+
+                        sells.Add(sell);
+                        if (!IsModel)
+                            db.Sells.Add(sell);
+
+                        product.Count--;
+                        sum += product.Price;
+                    }
+                }
+                if (!IsModel)
+                    db.SaveChanges();
+            }
+            return sum;
+        }
+    }
+}
---
+++ ProductSellTests/Model/CashTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProductSell.Model.Tests
+{
+    [TestClass()]
+    public class CashTests
+    {
+        [TestMethod()]
+        public void CashTest()
+        {
+            //arrange
+            var customer1 = new Customer()
+            {
+                Name = "testuser1",
+                CustomerID = 1
+            };
+            var customer2 = new Customer()
+            {
+                Name = "testuser2",
+                CustomerID = 2
+            };
+
+            var seller = new Seller()
+            {
+                Name = "sellername",
+                SellerID = 1
+            };
+            var product1 = new Product()
+            {
+                ProductID = 1,
+                Name = "pr1",
+                Price = 100,
+                Count = 10
+            };
+            var product2 = new Product()
+            {
+                ProductID = 2,
+                Name = "prod2",
+                Price = 200,
+                Count = 20
+            };
+
+            var cart1 = new Cart(customer1);
+            cart1.Add(product1);
+            cart1.Add(product1);
+            cart1.Add(product2);
+
+            var cart2 = new Cart(customer2);
+            cart2.Add(product1);
+            cart2.Add(product2);
+            cart2.Add(product2);
+
+            var cash = new Cash(1, seller);
+            cash.Unqueue(cart1);
+            cash.Unqueue(cart2);
+
+            var cart1ExectedResult = 400;
+            var cart2ExectedResult = 500;
+
+            //act
+            var cart1ActualResult = cash.Dequeue();
+            var cart2ActualResult = cash.Dequeue();
+
+            //assert
+            Assert.AreEqual(cart1ExectedResult, cart1ActualResult);
+            Assert.AreEqual(cart2ExectedResult, cart2ActualResult);
+            Assert.AreEqual(7, product1.Count);
+            Assert.AreEqual(17, product2.Count);
+        }
+    }
+}
